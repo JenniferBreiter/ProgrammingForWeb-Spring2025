327e91a7e92e0ac9cc43760c23ea6cefe54d0bae
Jennifer <jennifer.lbreiter@gmail.com>
2025-02-07 00:34:22 -0600

    update code

--- Week3/ex1/functiontile.js
+++ Week3/ex1/functiontile.js
@@ -1,38 +1,45 @@
 function setup() {
     createCanvas(200, 200);
-
+    noLoop();
 }
 function createTile(){
     translate(0, 0);
+    //background
     fill('#09122C');
     rect(0, 0, 200, 200);
-    line(0, 0, 200, 200);
-    line(200, 0, 0, 200);
-    line(0, 100, 100, 0);
-    line(0, 100, 200, 100);
-    line(200, 100, 100, 200);
-    line(0, 100, 100, 200);
-    line(100, 0, 200, 100);
+    //lines
     noFill();
     noStroke();
     stroke('#872341');
     strokeWeight(5);
-    circle(100, 100, 100);
+    line(0, 100, 100, 0);
+    line(100, 0, 200, 100);
+    line(0, 100, 100, 200);
+    line(100, 200, 200, 100);
+    //circles 
+    noFill();
+    noStroke();
+    stroke('#872341');
+    strokeWeight(5);
+    circle(100, 100, 105);
     circle(100, 100, 150);
+    circle(100, 100, 195);
+    //petals
     fill('#BE3144');
     circle(100, 50, 50);
     circle(50, 100, 50);
     circle(150, 100, 50);
     circle(100, 150, 50);
+    //center circle
     fill('#E17564');
-    circle(100, 100, 80);
-    noFill();
-    circle(100, 100, 200);
+    circle(100, 100, 70);
+    //corner circles
     fill('#E17564');
     circle(200, 200, 140);
     circle(0, 200, 140);
     circle(0, 0, 140);
     circle(200, 0, 140);
+    //inner corner circles
     fill('#BE3144');
     circle(200, 200, 100);
     circle(0, 0, 100);
