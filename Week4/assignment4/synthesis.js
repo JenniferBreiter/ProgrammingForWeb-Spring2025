f0bba65f0168ea4d6d12c9989d88811926a452ba
Jennifer <jennifer.lbreiter@gmail.com>
2025-02-17 01:09:23 -0600

    update

--- Week4/assignment4/synthesis.js
+++ Week4/assignment4/synthesis.js
@@ -1,33 +1,34 @@
-let myCard = [];
-let startingX = 25;
-let startingY = 50;
+const cards = [];
+let startingX = 75;
+let startingY = 200;
 let cardHeight = 400;
 let cardWidth = 250;
 let startingId = 0;
 
 function setup (){
-    createCanvas(1500, 1000);
-    background(150);
+    createCanvas(1500, 1500);
+    background('lightblue');
+
     // rectangle cards
-    for (let k = 0; k < 2; k++) {
+    for (let k = 0; k < 3; k++) {
         for (let i = 0; i < 5; i++){
             rect(startingX, startingY, cardWidth, cardHeight);
-            myCard.push({ x: startingX, y: startingY, id: startingId });
+            cards.push({ x: startingX, y: startingY, id: startingId });
             startingX += 275;
             startingId++;
         }
-        startingX = 25;
+        startingX = 75;
         startingY += 425; 
     }
     
-    console.log(myCard);
+    console.log(cards);
 }
 
 function mousePressed (){
-    for (let j = 0; j < myCard.length; j++) {
-        let distance = dist(mouseX, mouseY, myCard[j].x, myCard[j].y);
-        if (distance < cardWidth, distance < cardHeight) {
-            console.log('Card has been clicked');
+    for (let j = 0; j < cards.length; j++) {
+       let distance = dist(mouseX, mouseY, cards[j].x, cards[j].y);
+       if (distance < cardWidth / 2, distance < cardHeight / 2) {
+            console.log('Card has been clicked', cards[j].id);
         }
     }
 }
