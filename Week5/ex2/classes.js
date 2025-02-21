40c6ece21fff60bd3bfd5837dcc6bbadbc28eaf8
Jennifer <jennifer.lbreiter@gmail.com>
2025-02-21 02:05:05 -0600

    update ex 2

--- Week5/ex2/classes.js
+++ Week5/ex2/classes.js
@@ -1,7 +1,36 @@
+let bubble = [];
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 400);
+  for (let i = 0; i < bubble; i++) {
+    bubble = new Bubble();
+  }
+  
+  
 }
 
 function draw() {
-  background(220);
+  background(0);
+  for (let i = 0; i < bubble.length; i++) {
+     bubble[i].move();
+     bubble[i].show();
+  }
+ 
 }
+
+class Bubble (
+  constructor() {
+    this.x = 200;
+    this.y = 150;
+  }
+  move() {
+    this.x = this.x + random(-5, 5);
+    this.y = this.y + random(-5, 5);
+  }
+  show() {
+    stroke('pink');
+    strokeWeight(4);
+    noFill();
+    ellipse(this.x, this.y, 50, 50);
+  }
+)
