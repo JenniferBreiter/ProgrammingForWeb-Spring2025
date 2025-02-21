f6d123c92c79f7c8097171fbac016e2d5c08cbb2
Jennifer <jennifer.lbreiter@gmail.com>
2025-02-21 00:06:39 -0600

    update

--- Week5/ex1/timers.js
+++ Week5/ex1/timers.js
@@ -2,15 +2,15 @@ let blockX = 0;
 let blockY = 0;
 let blockColor = 255;
 let drawTimer;
-const speed = 20;
-const distance = 2; 
+const speed = 15;
+const distance = 3; 
 
 function setup() {
   createCanvas(500, 500);
   background(0);
 }
 
-function drawblock(x, y, color) {
+function drawBlock(x, y, color) {
   fill(color || 255);
   rect(x, y, 50, 50);
 }
@@ -26,10 +26,10 @@ function keyTyped() {
 
 }
 
-window.setTimeout(() => {
-  drawTimer = window.setInterval(() => {
-    if (blockY - 50 >= height) {
-      drawblock(blockX, blockY, blockColor);
+
+drawTimer = window.setInterval(() => {
+    if (blockY - 50 <= height) {
+      drawBlock(blockX, blockY, blockColor);
       blockY += distance;
     } else {
       blockY = 0;
@@ -37,10 +37,9 @@ window.setTimeout(() => {
     }
     if (blockY - 50 > height && blockX - 50 > width){
         window.clearInterval(drawTimer);
-        alert('done');
+        alert('Finished!');
     }
 
-  }, speed);
-}, 1500);
+}, speed);
 
 
